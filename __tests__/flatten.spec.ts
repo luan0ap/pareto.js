edea8e5b126ed955d58d7da716ea516b56f464b3
luan0ap <luanap.kun@gmail.com>
2018-06-07 14:50:48 -0300

    first commit

--- __tests__/flatten.spec.ts
+++
@@ -1,8 +0,0 @@
-import flatten from "../src/extra/flatten";
-
-it("flattens an array", () => {
-  expect(flatten([])).toEqual([]);
-  expect(flatten([1, 2])).toEqual([1, 2]);
-  expect(flatten([1, [2, 3]])).toEqual([1, 2, 3]);
-  expect(flatten([1, [2, 3], 4])).toEqual([1, 2, 3, 4]);
-});
