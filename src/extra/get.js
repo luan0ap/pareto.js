ccceed0042d0c875a2a9ea1f383a076106ff61b7
luan0ap <luanap.kun@gmail.com>
2018-06-19 17:09:16 -0300

    Refatored get function

--- src/extra/get.js
+++ src/extra/get.js
@@ -1,13 +1,1 @@
-export default function get(object, path, defaultValue = undefined) {
-  if (path) {
-    const pathSplitted = path.split('.')
-    const property = pathSplitted[0]
-    const findProperty = object[property]
-    if (findProperty && !path.includes('.')) {
-      return findProperty
-    }
-    const newPath = pathSplitted.slice(1, pathSplitted.length).join('.')
-    return get(findProperty, newPath, defaultValue)
-  }
-  return defaultValue
-}
+export default (object, path, defaultValue = undefined) => path.split('.').reduce((res, prop) => res[prop], object ) || defaultValue
