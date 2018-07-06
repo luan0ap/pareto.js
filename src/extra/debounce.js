a3027e4b6d21061d8d4ead1f68d670a0c8112f0e
Luan AP <luanap.kun@gmail.com>
2018-07-06 17:28:34 -0300

    Removed constant obj

--- src/extra/debounce.js
+++ src/extra/debounce.js
@@ -1,9 +1,8 @@
 const debounce = (fn, wait = 100, immediate = false) => (...args) => {
-  const obj = this
   let timeout
 
   const delayed = () => {
-    !immediate && fn.apply(obj, args)
+    !immediate && fn.apply(this, args)
     timeout = null
   }
 
